dc3db633b34aee018e46a891014aabdcb7d86b36
denise <denise.eliza@hey.com>
2022-12-03 10:56:13 -0500

    day 3 parts 1 and 2 using set intersections

---
+++ day3.py
@@ -0,0 +1,44 @@
+filename = 'inputs/day3.txt'
+#filename = 'inputs/test.txt'
+
+with open(filename) as f:
+    lines = [line.rstrip() for line in f]
+
+
+def get_priority(c):
+    # A function to convert single characters into priorities
+    if c.isupper():
+        return ord(c) - 38
+    return ord(c) - 96
+
+
+elf_counter = 0
+elf_group = []
+priority_p1 = 0
+priority_p2 = 0
+
+for this_line in lines:
+    # Part 1
+
+    # Turning things into sets removes all duplicates
+    c_1 = set(this_line[:len(this_line)//2])
+    c_2 = set(this_line[len(this_line)//2:])
+
+    # Find intersection of the sets to get the repeated item
+    # next(iter(set_name)) returns the first (only) item in the set
+    dup_p1 = next(iter(c_1.intersection(c_2)))
+    priority_p1 += get_priority(dup_p1)
+
+    # Part 2
+    elf_group.append(this_line)
+    if elf_counter == 2:
+        # Once we have a set of 3 elves, convert list items to sets and find the intersection
+        dup_p2 = next(iter(set(elf_group[0]).intersection(set(elf_group[1]), set(elf_group[2]))))
+        priority_p2 += get_priority(dup_p2)
+        elf_counter = 0
+        elf_group = []
+    else:
+        elf_counter += 1
+
+print("Part 1:", priority_p1)
+print("Part 2:", priority_p2)
