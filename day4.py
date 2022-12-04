c966c4113f5c9ff08ebb991b724553723cf565a8
denise <denise.eliza@hey.com>
2022-12-04 13:00:24 -0500

    day 4 parts 1 and 2 with sets

--- day4.py
+++ day4.py
@@ -4,15 +4,14 @@ filename = 'inputs/day4.txt'
 #filename = 'inputs/test.txt'
 
 with open(filename) as f:
-    lines = [line.rstrip() for line in f]
+    lines = [list(map(int, re.split('-|,', line.rstrip()))) for line in f]
 
 overlap_count_1 = 0
 overlap_count_2 = 0
 
 for this_pair in lines:
-    pairs = [int(f) for f in re.split('-|,', this_pair)]
-    elf_1 = set([n for n in range(pairs[0], pairs[1] + 1)])
-    elf_2 = set([n for n in range(pairs[2], pairs[3] + 1)])
+    elf_1 = set([n for n in range(this_pair[0], this_pair[1] + 1)])
+    elf_2 = set([n for n in range(this_pair[2], this_pair[3] + 1)])
 
     if elf_1.issubset(elf_2) or elf_2.issubset(elf_1):
         overlap_count_1 += 1
