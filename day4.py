f5cd69774023f500de8d697defb5d05cb1d115ff
denise <denise.eliza@hey.com>
2022-12-04 12:30:17 -0500

    day 4 parts 1 and 2 with sets

--- day4.py
+++ day4.py
@@ -11,22 +11,13 @@ overlap_count_2 = 0
 
 for this_pair in lines:
     pairs = [int(f) for f in re.split('-|,', this_pair)]
-    elf_1 = [pairs[0], pairs[1]]
-    elf_2 = [pairs[2], pairs[3]]
+    elf_1 = set([n for n in range(pairs[0], pairs[1] + 1)])
+    elf_2 = set([n for n in range(pairs[2], pairs[3] + 1)])
 
-    if elf_1[0] >= elf_2[0] and elf_1[1] <= elf_2[1]:
+    if elf_1.issubset(elf_2) or elf_2.issubset(elf_1):
         overlap_count_1 += 1
-        overlap_count_2 += 1
-    elif elf_2[0] >= elf_1[0] and elf_2[1] <= elf_1[1]:
-        overlap_count_1 += 1
-        overlap_count_2 += 1
-    elif elf_1[0] <= elf_2[0] <= elf_1[1]:
-        overlap_count_2 += 1
-    elif elf_1[0] <= elf_2[1] <= elf_1[1]:
-        overlap_count_2 += 1
-    elif elf_2[0] <= elf_1[0] <= elf_2[1]:
-        overlap_count_2 += 1
-    elif elf_2[0] <= elf_1[1] <= elf_2[1]:
+
+    if not elf_1.isdisjoint(elf_2):
         overlap_count_2 += 1
 
 print("Part 1:", overlap_count_1)
