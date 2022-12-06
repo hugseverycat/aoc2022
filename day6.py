cab86aaa0f72f28256b3dcbc465d3e49a67352f9
denise <denise.eliza@hey.com>
2022-12-06 18:00:54 -0500

    day 6 parts 1 and 2 minor edit

--- day6.py
+++ day6.py
@@ -1,14 +1,16 @@
 filename = 'inputs/day6.txt'
-#filename = 'inputs/test.txt'
+filename = 'inputs/test.txt'
 
 with open(filename) as f:
     signal = f.readline()
 
 buffer = ''
+b_len = 4   # Part 1: 4; Part 2: 14
+
 for i, c in enumerate(signal):
     buffer += c
-    if len(buffer) == 14:           # Switch to 4 for part 1
-        if len(set(buffer)) == 14:  # Switch to 4 for part 1
+    if len(buffer) == b_len:
+        if len(set(buffer)) == b_len:
             marker = i + 1
             break
         else:
