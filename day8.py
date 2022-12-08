251271590db9928cb4815785f6047c0f9327a996
denise <denise.eliza@hey.com>
2022-12-08 14:30:58 -0500

    day 8 parts 1 and 2

--- day8.py
+++ day8.py
@@ -1,5 +1,5 @@
 filename = 'inputs/day8.txt'
-filename = 'inputs/test.txt'
+#filename = 'inputs/test.txt'
 
 with open(filename) as f:
     lines = [line.rstrip() for line in f]
@@ -13,92 +13,63 @@ for y, this_row in enumerate(lines):
     for x, this_col in enumerate(this_row):
         tree_map[(x, y)] = this_col
 
-
-def is_visible(coord, t_map, h, w):
-    cx = coord[0]
-    cy = coord[1]
-    visible = 4
-    scenic_score = 1
-
-    if cx in (0, w-1) or cy in (0, h-1):
-        #print(coord, "is on the edge")
-        scenic_score = 0
-    else:
-        print(coord, "is in the interior")
-        temp_score = 1
-        for x in range(cx+1, w-1):
-            if t_map[(x, cy)] >= t_map[coord]:
-                visible -= 1
-                break
-            temp_score += 1
-        scenic_score *= temp_score
-        if coord == debug_coord:
-            print(coord, "sees", temp_score, "to the right")
-            print("---")
-
-        temp_score = 1
-        for y in range(cy+1, h-1):
-            if t_map[(cx, y)] >= t_map[coord]:
-                visible -= 1
-                break
-            temp_score += 1
-        scenic_score *= temp_score
-        if coord == debug_coord:
-            print(coord, "sees", temp_score, "to the bottom")
-            print("---")
-
-        temp_score = 1
-        for x in range(cx-1, 0, -1):
-            if t_map[(x, cy)] >= t_map[coord]:
-                visible -= 1
-                break
-            temp_score += 1
-        scenic_score *= temp_score
-        if coord == debug_coord:
-            print(coord, "sees", temp_score, "to the left")
-            print("---")
-
-        temp_score = 1
-        for y in range(cy-1, 0, -1):
-            if t_map[(cx, y)] >= t_map[coord]:
-                visible -= 1
-                break
-            temp_score += 1
-        scenic_score *= temp_score
-        if coord == debug_coord:
-            print(coord, "sees", temp_score, "to the top")
-            print("---")
-
-        if visible:
-            print(visible)
-            print(coord, "is visible")
-            print("---")
-        else:
-            print(visible)
-            print(coord, "is not visible")
-            print("---")
-
-    return (visible, scenic_score)
-
-
 visible_count = 0
-max_scenic_score = 0
-max_coord = None
-for c in tree_map:
-    v, s = is_visible(c, tree_map, height, width)
-    if v:
-        visible_count += 1
-    if s > max_scenic_score:
-        max_scenic_score = s
+max_scenic = 0
+
+for coord in tree_map:
+    cx, cy = coord
+
+    # Detect whether it is on an edge
+    if cx in (0, width - 1) or cy in (0, height - 1):
+        visible_count += 1  # Edge trees are automatically visible
+                            # and have a scenic value of zero
+    else:  # We're an inner tree
+        right = 0
+        right_visible = True
+        for x in range(cx+1, width):  # Checking to the right
+            if tree_map[coord] <= tree_map[x, cy]:
+                right_visible = False
+                right += 1
+                break
+            else:
+                right += 1
+
+        left = 0
+        left_visible = True
+        for x in range(cx-1, -1, -1):  # Checking to the left
+            if tree_map[coord] <= tree_map[x, cy]:
+                left_visible = False
+                left += 1
+                break
+            else:
+                left += 1
+
+        up = 0
+        up_visible = True
+        for y in range(cy-1, -1, -1):  # Checking to the up
+            if tree_map[coord] <= tree_map[cx, y]:
+                up_visible = False
+                up += 1
+                break
+            else:
+                up += 1
+
+        down = 0
+        down_visible = True
+        for y in range(cy+1, height):  # Checking to the down
+            if tree_map[coord] <= tree_map[cx, y]:
+                down_visible = False
+                down += 1
+                break
+            else:
+                down += 1
+
+        if up_visible or down_visible or left_visible or right_visible:
+            visible_count += 1
+
+        scenic_value = up * down * left * right
+        if scenic_value > max_scenic:
+            max_scenic = scenic_value
 
 print("Part 1:", visible_count)
-print("Part 2:", max_scenic_score)
-print(max_coord)
-
-
-
-"""for y in range(len(lines)):
-    print_line = ''
-    for x in range(len(lines[0])):
-        print_line += tree_map[(x, y)]
-    print(print_line)"""
+print("Part 2:", max_scenic)
