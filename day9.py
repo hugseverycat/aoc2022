ceaf0f5330c83f989713e2b9c3c4e0e95e6cf3c7
denise <denise.eliza@hey.com>
2022-12-09 12:39:27 -0500

    day 9 part 1 and 2 (messy)

--- day9.py
+++ day9.py
@@ -6,17 +6,6 @@ filename = 'inputs/day9.txt'
 with open(filename) as f:
     lines = [line.rstrip().split(' ') for line in f]
 
-moves = []
-head_location = (0, 0)
-tail_location = (0, 0)
-
-head_visited = defaultdict(int)
-tail_visited = defaultdict(int)
-head_visited[head_location] = 1
-tail_visited[tail_location] = 1
-
-for this_line in lines:
-    moves.append([this_line[0], int(this_line[1])]) # R 17
 
 
 def move_head(direction, c_pos):
@@ -46,28 +35,83 @@ def move_tail(t_pos, h_pos):
             move_direction.append(p//abs(p))
     return tuple([current + increase for current, increase in zip(t_pos, move_direction)])
 
+def do_part1():
+    moves = []
+    head_location = (0,0)
+    tail_location = (0, 0)
+    head_visited = defaultdict(int)
+    tail_visited = defaultdict(int)
+    head_visited[head_location] = 1
+    tail_visited[tail_location] = 1
 
-#print(move_head('U', (3, 3)))
-#print(move_tail((0,0), (3, 7)))
-
-for this_move in moves:
-    move_dir, total_steps = this_move
-    #print(f"Starting move: {this_move}. Head: {head_location}; Tail: {tail_location}")
-    for i in range(total_steps):
-        head_location = move_head(move_dir, head_location)
-        #print(f"  Head moved to {head_location}")
-        new_tail = move_tail(tail_location, head_location)
-        if new_tail != tail_location:
-            tail_location = new_tail
-            tail_visited[tail_location] += 1
-            #print(f"  Tail followed to {tail_location}")
-        else:
-            #print(f"  Tail at {tail_location} did not move")
-            pass
-    #print("--------")
+    for this_line in lines:
+        moves.append([this_line[0], int(this_line[1])]) # R 17
 
 
-"""for i, t in enumerate(tail_visited):
-    print(f"{i+1}. {t}: {tail_visited[t]}")"""
 
-print(f"Part 1: {len(tail_visited)}")
+    #print(move_head('U', (3, 3)))
+    #print(move_tail((0,0), (3, 7)))
+
+    for this_move in moves:
+        move_dir, total_steps = this_move
+        #print(f"Starting move: {this_move}. Head: {head_location}; Tail: {tail_location}")
+        for i in range(total_steps):
+            head_location = move_head(move_dir, head_location)
+            #print(f"  Head moved to {head_location}")
+            new_tail = move_tail(tail_location, head_location)
+            if new_tail != tail_location:
+                tail_location = new_tail
+                tail_visited[tail_location] += 1
+                #print(f"  Tail followed to {tail_location}")
+            else:
+                #print(f"  Tail at {tail_location} did not move")
+                pass
+        #print("--------")
+
+
+    """for i, t in enumerate(tail_visited):
+        print(f"{i+1}. {t}: {tail_visited[t]}")"""
+
+    print(f"Part 1: {len(tail_visited)}")
+
+
+def do_part2():
+    moves = []
+    num_knots = 10
+    knot_list = [(0, 0) for _ in range(num_knots)]
+    tail_visited = defaultdict(int)
+    tail_visited[knot_list[-1]] = 1
+
+    for this_line in lines:
+        moves.append([this_line[0], int(this_line[1])]) # R 17
+
+    #print(move_head('U', (3, 3)))
+    #print(move_tail((0,0), (3, 7)))
+
+    for this_move in moves:
+        move_dir, total_steps = this_move
+        #print(f"Starting move: {this_move}. Head: {head_location}; Tail: {tail_location}")
+        for _ in range(total_steps):
+            knot_list[0] = move_head(move_dir, knot_list[0])
+            for i in range(1, num_knots):
+                new_knot_position = move_tail(knot_list[i], knot_list[i-1])
+                if new_knot_position != knot_list[i]:
+                    knot_list[i] = new_knot_position
+                    if i == num_knots - 1:
+                        tail_visited[knot_list[i]] += 1
+                    #print(f"  Tail followed to {tail_location}")
+                else:
+                    #print(f"  Tail at {tail_location} did not move")
+                    pass
+        #print("--------")
+
+
+    """for i, t in enumerate(tail_visited):
+        print(f"{i+1}. {t}: {tail_visited[t]}")"""
+
+    print(f"Part 2: {len(tail_visited)}")
+
+
+
+do_part1()
+do_part2()
