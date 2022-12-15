7c27f8b642f3251203d05cefb5f0329ad54426bd
denise <denise.eliza@hey.com>
2022-12-15 16:13:01 -0500

    day 15 parts 1 and 2 (it's all messy)

--- day15.py
+++ day15.py
@@ -13,10 +13,9 @@ def manhattan_distance(start_coord: tuple, end_coord: tuple):
 
 sensor_ranges = {}
 beacon_positions = set()
-# Sensor at x=3163363, y=3448163: closest beacon is at x=3102959, y=3443573
+
 min_x = min_y = None
 max_x = max_y = 0
-sensor_in_range = set()
 
 for this_line in input_lines:
     sx, sy, bx, by = [int(n) for n in re.findall("=(-?\d+)", this_line)]
@@ -33,13 +32,15 @@ for this_line in input_lines:
         max_x = sx + sensor_ranges[(sx, sy)]
 
 
-print(min_x, max_x, min_y, max_y)
+# set of coords that are "occupied"
 beacons_and_sensors = beacon_positions.union(set([s for s in sensor_ranges]))
-print(len(sensor_in_range))
 
 # Part 1
 check_line = 2000000
 counter = 0
+
+# naive approach: for all the x's in y = checkline, check if its within the range
+# of each sensor 1 by 1
 for x in range(min_x, max_x + 1):
     in_range = False
     for this_sensor in sensor_ranges:
@@ -50,9 +51,56 @@ for x in range(min_x, max_x + 1):
             break
     if in_range:
         counter += 1
+print(f"Part 1: {counter}")
 
-print(counter)
+
+def walk_perimeter(this_sensor: tuple, s_positions:dict):
+    # For part 2 we get which points are just outside each sensor's range
+    # The beacon must be on one of these perimeter points
+    sx, sy = this_sensor
+    s_range = s_positions[this_sensor]
+    perimeter_coords = set()
+    cx = sx - s_range - 1
+    cy = sy
+    while cx < sx:
+        perimeter_coords.add((cx, cy))
+        cx += 1
+        cy += 1
+    while cy > sy:
+        perimeter_coords.add((cx, cy))
+        cx += 1
+        cy -= 1
+    while cx > sx:
+        perimeter_coords.add((cx,cy))
+        cx -= 1
+        cy += 1
+    while cy > sy:
+        perimeter_coords.add((cx, cy))
+        cx -= 1
+        cy -= 1
+
+    return perimeter_coords
 
 
-# wrong answers
-# 4984495 (too high)
+for s in sensor_ranges:
+    perimeter = walk_perimeter(s, sensor_ranges)
+    hidden = False
+
+    # For each point on the perimeter, check if its in the target zone
+    # then check it against the manhattan distance of each sensor
+    # if its not within any sensor's range, hooray, we're done!!
+    for p in perimeter:
+        x, y = p
+        if 0 <= x <= 4000000 and 0 <= y <= 4000000:
+            hidden = True
+            for this_sensor in sensor_ranges:
+                if manhattan_distance(p, this_sensor) <= sensor_ranges[this_sensor]:
+                    hidden = False
+                    break
+            if hidden:
+                distress_beacon = p
+                break
+    if hidden:
+        break
+
+print(f"Part 2: {4000000 * distress_beacon[0] + distress_beacon[1]}")
